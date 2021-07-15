1c59a5324d5dd29d72f7eefb29cd2f5e658306a6
radoneje <den.shevchenko@gmail.com>
2021-07-15 20:41:10 +0500

    ddd

--- routes/nfupload.js
+++ routes/nfupload.js
@@ -3,7 +3,9 @@ var express = require('express');
 var router = express.Router();
 
 router.post("/",(req,res)=>{
-    console.log("w", req.body)
+
+    var arr=JSON.parse(req.body.titles)
+    console.log("arr", req.body.titles)
     res.json(2);
 })
 
