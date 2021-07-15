dd9977493fd259d5b61a9d7134e956b7b2e9d735
radoneje <den.shevchenko@gmail.com>
2021-07-15 19:54:06 +0500

    ddd

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.get('/', (req, res) => {
+app.get('/nfupload', (req, res) => {
     res.json(1)
 })
+
+app.get('/', (req, res) => {
+    res.send('ok')
+})
+
 
 
 app.listen(port, () => {
