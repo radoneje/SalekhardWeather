2872fcae3bbfbc5986c53f05654d46d5780bb450
radoneje <den.shevchenko@gmail.com>
2021-12-02 20:42:57 +0300

    ddd

--- routes/nfupload.js
+++ routes/nfupload.js
@@ -16,55 +16,55 @@ router.post("/",async (req,res)=>{
     const workbook = new ExcelJS.Workbook();
     const sheet = workbook.addWorksheet('NF');
     var i = 1;
-    console.log("arr loop")
-    var buf=[[],[],[],[],[]]
-    arr.forEach(elem=> {
-        if (elem.type == 'SOT'){
-            buf[0].push(elem.name)
-            buf[1].push(elem.pos)
-        }
-        if (elem.type == 'THM')
-            buf[2].push(elem.text)
-        if (elem.type == 'GEO')
-            buf[3].push(elem.text)
-        if (elem.type == 'SRC')
-            buf[4].push(elem.text)
-    })
+    console.log("arr loop", arr);
+    /* var buf=[[],[],[],[],[]]
+     arr.forEach(elem=> {
+         if (elem.type == 'SOT'){
+             buf[0].push(elem.name)
+             buf[1].push(elem.pos)
+         }
+         if (elem.type == 'THM')
+             buf[2].push(elem.text)
+         if (elem.type == 'GEO')
+             buf[3].push(elem.text)
+         if (elem.type == 'SRC')
+             buf[4].push(elem.text)
+     })
 
-    var values=[];
-    var l=Math.max(buf[0].length, buf[1].length, buf[2].length, buf[3].length,buf[4].length)
-    for(var i=1; i<=l;i++){
+     var values=[];
+     var l=Math.max(buf[0].length, buf[1].length, buf[2].length, buf[3].length,buf[4].length)
+     for(var i=1; i<=l;i++){
 
-        buf.forEach(b=>{
-            if(b.length<i-1)
-                values.push(b)
-            //else
-                //values.push("")
-        })
+         buf.forEach(b=>{
+             if(b.length<i-1)
+                 values.push(b)
+             //else
+                 //values.push("")
+         })
 
-    }
-    console.log("buf ", values,values.length );
-    for(var i=1;i<=values.length; i++)
-    {
-        let row = sheet.getRow(i)
-        console.log(i, values[i-1])
-        row.values =values[i-1];
-    }
+     }
+     console.log("buf ", values,values.length );
+     for(var i=1;i<=values.length; i++)
+     {
+         let row = sheet.getRow(i)
+         console.log(i, values[i-1])
+         row.values =values[i-1];
+     }
 
 
-   /* arr.forEach(elem=> {
-        let row = sheet.getRow(i)
-        i++;
-        if (elem.type == 'SOT')
-            row.values = [elem.name, elem.pos,"","",""]
-        if (elem.type == 'THM')
-            row.values = ["", "", elem.text,"",""]
-        if (elem.type == 'GEO')
-            row.values = ["", "", "", elem.text,""]
-        if (elem.type == 'SRC')
-            row.values = ["", "", "", "", elem.text]
-        console.log("arr elem", i, row.values)
-    });*/
+    /* arr.forEach(elem=> {
+         let row = sheet.getRow(i)
+         i++;
+         if (elem.type == 'SOT')
+             row.values = [elem.name, elem.pos,"","",""]
+         if (elem.type == 'THM')
+             row.values = ["", "", elem.text,"",""]
+         if (elem.type == 'GEO')
+             row.values = ["", "", "", elem.text,""]
+         if (elem.type == 'SRC')
+             row.values = ["", "", "", "", elem.text]
+         console.log("arr elem", i, row.values)
+     });*/
     console.log("make xlsx")
         await workbook.xlsx.writeFile("/tmp/nf.xlsx")
     console.log("convert xlsx")
